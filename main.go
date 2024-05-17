6fae3a823beb64d18d0a62f21518a6d4b900a79b
gptjddldi <hystom38@gmail.com>
2024-05-17 18:42:50 +0900

    Integrate workload generator with my lsm tree

--- main.go
+++ main.go
@@ -3,28 +3,26 @@ package main
 import (
 	"bufio"
 	"fmt"
+	Lsm "github.com/gptjddldi/lsm"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
+	"time"
 )
 
-// C++ 프로그램 실행
 func runGenerator() error {
 	generatorPath := "./lsm-tree/generator1"
 
-	// 실행 권한 부여
-	if err := os.Chmod(generatorPath, 0755); err != nil {
-		return fmt.Errorf("failed to set executable permission: %w", err)
-	}
-
 	cmd := exec.Command(generatorPath,
 		"--puts", "100000",
-		"--gets", "1000",
-		"--ranges", "10",
+		//"--gets", "1000",
+		//"--ranges", "10",
 		"--deletes", "20",
-		"--gets-misses-ratio", "0.3",
-		"--gets-skewness", "0.2")
+		//"--gets-misses-ratio", "0.3",
+		//"--gets-skewness", "0.2",
+	)
 
-	// 생성된 파일로 출력 리디렉션
 	outfile, err := os.Create("workload.txt")
 	if err != nil {
 		return err
@@ -34,42 +32,86 @@ func runGenerator() error {
 	cmd.Stdout = outfile
 	cmd.Stderr = os.Stderr
 
-	// 명령어 실행
 	return cmd.Run()
 }
 
-// 생성된 파일 읽기
-func readWorkload(file string) error {
-	f, err := os.Open(file)
+const dataFolder = "demo-data"
+
+func eraseDataFolder() {
+	err := os.RemoveAll(dataFolder)
 	if err != nil {
-		return err
+		panic(err)
 	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		return err
-	}
-
-	return nil
 }
-
 func main() {
-	// C++ 프로그램 실행
+	eraseDataFolder()
+
+	// workload generator 실행
 	err := runGenerator()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error running generator: %v\n", err)
 		os.Exit(1)
 	}
 
-	// 생성된 파일 읽기
-	err = readWorkload("workload.txt")
+	db, err := Lsm.Open(dataFolder)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error reading workload: %v\n", err)
+		log.Fatal(err)
+	}
+
+	file, err := os.Open("workload.txt")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
 		os.Exit(1)
 	}
+	defer file.Close()
+
+	start := time.Now()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+		if len(parts) == 0 {
+			continue
+		}
+
+		command := parts[0]
+		switch command {
+		case "p":
+			if len(parts) != 3 {
+				continue
+			}
+			key := []byte(parts[1])
+			value := []byte(parts[2])
+			db.Insert(key, value)
+		case "g":
+			if len(parts) != 2 {
+				continue
+			}
+			key := []byte(parts[1])
+			db.Get(key)
+		//case "r":
+		//	if len(parts) != 3 {
+		//		continue
+		//	}
+		//	start, _ := strconv.Atoi(parts[1])
+		//	end, _ := strconv.Atoi(parts[2])
+		//	db.Range(start, end)
+		case "d":
+			if len(parts) != 2 {
+				continue
+			}
+			key := []byte(parts[1])
+			db.Delete(key)
+		default:
+			fmt.Fprintf(os.Stderr, "Unknown command: %s\n", command)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+	}
+
+	elapsed := time.Since(start)
+	fmt.Printf("Command execution time: %s\n", elapsed)
 }
